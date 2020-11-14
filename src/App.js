40d6f092cb5fe80d17001bdfedd4b5f2aa57e7b2
Anggara Diebrata <angga.deebrata@gmail.com>
2020-11-14 16:35:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { useState } from 'react';
-import './App.css';
-import ReactSS, {ReactNoJSX} from './components/ReactJSX';
-import CompAndProps from './components/CompAndProps';
-// import Button from './elements/Button';
-import TextInput from './elements/TextInput';
-import {notification, Button} from 'antd'
-
+import React, { useState } from "react";
+import "./App.css";
+import ReactSS, { ReactNoJSX } from "./components/ReactJSX";
+import CompAndProps from "./components/CompAndProps";
+import Button from "./elements/Button";
+import TextInput from "./elements/TextInput";
 
 function App() {
+  const onClickButton = (e) => {
+    alert("Haii");
+  };
+
+  const [data, setData] = useState();
+  const onChange = (e) => {
+    setData(e.target.value);
+  };
   return (
     <div className="App">
       <div className="App-header">
         <ReactSS />
         {/* <ReactNoJSX /> */}
         <div className="Components">
-          <CompAndProps bgcolor="#66ccff"/>
-          <TextInput border="red"/>
+          <CompAndProps bgcolor="#66ccff" />
+          <TextInput border="green" value={data} onChange={onChange} />
+          <Button background="red" onClick={onClickButton}>
+            Hello
+          </Button>
+          <p>{data}</p>
         </div>
+        x
       </div>
     </div>
   );
